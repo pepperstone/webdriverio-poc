6d02cd47040d778d291a029411d44c3f7389eac5
Bashir Towdiee <bash.towdiee@pepperstone.com>
2022-06-08 14:38:59 +1000

    feat(theme): adding official darkmode theme palette.

---
+++ packages/app/src/lib/theme/coreTheme/darkThemePalette.ts
@@ -0,0 +1,89 @@
+export default {
+	primary: '#000000',
+	background: '#000000',
+	card: '#000000',
+	text: '#FFFFFF',
+	border: '#CCCCCC',
+	notification: '#000000',
+	product: {
+		brand: {
+			primary: '#457AFF',
+			secondary: '#000000',
+			destructive: '#FF6F65',
+		},
+		background: {
+			primaryBase: '#457AFF',
+			primarySecondary: '#E6E6E6',
+			secondary: '#03142E',
+			base: '#000B1C',
+		},
+		button: {
+			primary: {
+				default: '#457AFF',
+				hover: '#668EFF',
+			},
+			secondary: {
+				default: '#E8E8E8',
+				hover: '#E8E8E8',
+			},
+			primaryOnBlue: {
+				default: '#FFFFFF',
+				hover: '#F7F7F7',
+			},
+			secondaryOnBlue: {
+				default: '#E8E8E8',
+				hover: '#E8E8E8',
+			},
+			success: {
+				default: '#3DC931',
+				hover: '#8EF568',
+			},
+			outline: {
+				default: '#9C9C9C',
+				hover: '#B9B9B9',
+			},
+			selected: {
+				default:'#FFFFFF',
+			},
+			destructive: {
+				default: '#FF6F65',
+				hover: '#FF8779',
+			},
+			text: {
+				strong: '#151515',
+				subdued: '#FFFFFF',
+				disabled: '#808080',
+				primaryOnBlue: '#151515',
+			},
+		},
+		text: {
+			strong: '#FFFFFF',
+			subdued: '#8A9DBA',
+			blue: '#151515',
+		},
+		icon: {
+			twoTone: {
+				primary: '#457AFF',
+				secondary: '#C1D3FF',
+				primaryOnBlue: '#FFFFFF',
+				secondaryOnBlue: '#9DB9FF',
+			},
+		},
+		border: {
+			primary: '#457AFF',
+			secondary: '#192940',
+			destructive: '#FF6F65',
+			success: '#77DE52',
+		},
+		forms: {
+			border: {
+				default: '#4C4C4C',
+				focused: '#457AFF',
+				error: '#FF6F65',
+			},
+			bg: {
+				default: '#E8E8E8',
+			},
+		},
+	},
+};
---
+++ packages/app/src/lib/theme/coreTheme/lightThemePalette.ts
@@ -0,0 +1,90 @@
+export default {
+	primary: '#FFFFFF',
+	background: '#FFFFFF',
+	card: '#F7F7F7',
+	text: '#000000',
+	border: '#999999',
+	notification: '#FFFFFF',
+
+	product: {
+		brand: {
+			primary: '#0064FA',
+			secondary: '#151515',
+			destructive: '#DF1D2C',
+		},
+		background: {
+			primaryBase: '#0064FA',
+			primarySecondary: '#DADADA',
+			secondary: '#F7F7F7',
+			base: '#FFFFFF',
+		},
+		button: {
+			primary: {
+				default: '#0064FA',
+				hover: '#0054E6',
+			},
+			secondary: {
+				default: '#DADADA',
+				hover: '#CFCFCF',
+			},
+			primaryOnBlue: {
+				default: '#FFFFFF',
+				hover: '#F7F7F7',
+			},
+			secondaryOnBlue: {
+				default: '#E8E8E8',
+				hover: '#E8E8E8',
+			},
+			success: {
+				default: '#008500',
+				hover: '#007000',
+			},
+			outline: {
+				default: '#9C9C9C',
+				hover: '#808080',
+			},
+			selected: {
+				default:'#151515',
+			},
+			destructive: {
+				default: '#DF1D2C',
+				hover: '#C5001A',
+			},
+			text: {
+				strong: '#FFFFFF',
+				subdued: '#151515',
+				disabled: '#9C9C9C',
+				primaryOnBlue: '#FFFFFF',
+			},
+		},
+		text: {
+			strong: '#151515',
+			subdued: '#656565',
+			blue: '#FFFFFF',
+		},
+		icon: {
+			twoTone: {
+				primary: '#0064FA',
+				secondary: '#639FF8',
+				primaryOnBlue: '#FFFFFF',
+				secondaryOnBlue: '#9DB9FF',
+			},
+		},
+		border: {
+			primary: '#0064FA',
+			secondary: '#D6D6D6',
+			destructive: '#DF1D2C',
+			success: '#008500',
+		},
+		forms: {
+			border: {
+				default: '#D6D6D6',
+				focused: '#0064FA',
+				error: '#DF1D2C',
+			},
+			bg: {
+				default: '#E0E0E0',
+			},
+		},
+	},
+};
