a0380ec75241b6bcec9eb537189751a1469817d1
Bashir Towdiee <bash.towdiee@pepperstone.com>
2022-06-20 11:05:23 +1000

    refactor(bottomnavigation): update properties and simplify tab component usage

--- packages/app/src/common/components/bottomTabNavigationBar/BottomTabNavigationBar.tsx
+++ packages/app/src/common/components/bottomTabNavigationBar/BottomTabNavigationBar.tsx
@@ -8,7 +8,7 @@ import {
 } from 'assets/icons';
 import React, { useCallback, useMemo } from 'react';
 import { View } from 'react-native';
-import { useTheme } from '../../../lib/theme/Theme';
+import { useTheme } from 'src/lib/theme/Theme';
 import stylesCreator from './Styles';
 import Tab from './tab';
 
@@ -23,67 +23,50 @@ const BottomTabNavigationBar = ({ navigation, state }: BottomTabBarProps) => {
 
   const activeTab = useMemo(() => state.routes[state.index].name, [state]);
 
-  const activeColor = (name: string) =>
-    activeTab === name
-      ? theme.colorsInvert.product.brand.primary
-      : theme.colors.product.text.strong;
+  const activeColor = useCallback(
+    (name: string) =>
+      activeTab === name
+        ? theme.colorsInvert.product.brand.primary
+        : theme.colors.product.text.strong,
+    [theme, activeTab],
+  );
 
   return (
     <View style={styles.tabWrapper}>
       <Tab
         name="Discover"
         onTabPress={handleTabPress}
-        textColor={activeColor('Discover')}
-        tabIcon={
-          <DiscoverSVG
-            fill={theme.colors.product.background.base}
-            fillSecondary={activeColor('Discover')}
-          />
-        }
+        Icon={DiscoverSVG}
+        fill={theme.colors.product.background.base}
+        activeColor={activeColor('Discover')}
       />
       <Tab
         name="Watchlists"
         onTabPress={handleTabPress}
-        textColor={activeColor('Watchlists')}
-        tabIcon={
-          <WatchlistsSVG
-            fill={theme.colors.product.background.base}
-            fillSecondary={activeColor('Watchlists')}
-          />
-        }
+        Icon={WatchlistsSVG}
+        fill={theme.colors.product.background.base}
+        activeColor={activeColor('Watchlists')}
       />
       <Tab
         name="Search"
         onTabPress={handleTabPress}
-        textColor={activeColor('Search')}
-        tabIcon={
-          <SearchSVG
-            fill={theme.colors.product.background.base}
-            fillSecondary={activeColor('Search')}
-          />
-        }
+        Icon={SearchSVG}
+        fill={theme.colors.product.background.base}
+        activeColor={activeColor('Search')}
       />
       <Tab
         name="Markets"
         onTabPress={handleTabPress}
-        textColor={activeColor('Markets')}
-        tabIcon={
-          <MarketsSVG
-            fill={theme.colors.product.background.base}
-            fillSecondary={activeColor('Markets')}
-          />
-        }
+        Icon={MarketsSVG}
+        fill={theme.colors.product.background.base}
+        activeColor={activeColor('Markets')}
       />
       <Tab
         name="Portfolio"
         onTabPress={handleTabPress}
-        textColor={activeColor('Portfolio')}
-        tabIcon={
-          <PortfolioSVG
-            fill={theme.colors.product.background.base}
-            fillSecondary={activeColor('Portfolio')}
-          />
-        }
+        Icon={PortfolioSVG}
+        fill={theme.colors.product.background.base}
+        activeColor={activeColor('Portfolio')}
       />
     </View>
   );
--- packages/app/src/common/components/bottomTabNavigationBar/tab/Styles.ts
+++ packages/app/src/common/components/bottomTabNavigationBar/tab/Styles.ts
@@ -1,15 +1,15 @@
 import { StyleSheet } from 'react-native';
-import { StylesCreator } from '../../../../lib/theme/Theme';
+import { StylesCreator } from 'src/lib/theme/Theme';
 import TabProps from './types';
 
-const stylesCreator: StylesCreator = (theme, scale, moderateScale, props: TabProps ) => StyleSheet.create({
+const stylesCreator: StylesCreator = (theme, scale, _, props: TabProps ) => StyleSheet.create({
   tab: {
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
   },
   text: {
-    color: props.textColor,
+    color: props.activeColor,
     fontFamily: theme.font.regular,
     fontSize: scale(theme.fontSize.s1),
     fontWeight: theme.fontWeight.semibold,
