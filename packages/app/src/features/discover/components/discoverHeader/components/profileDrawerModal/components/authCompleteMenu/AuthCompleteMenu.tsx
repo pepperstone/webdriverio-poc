7254e23c985f9edea214e66f4bf36ed55a6b7da2
gabriel mori baleta <>
2022-06-23 15:04:49 +0800

    fix( removed activity indicator ): removed the activity indicator inside AuthCompleteMenu

--- packages/app/src/features/discover/components/discoverHeader/components/profileDrawerModal/components/authCompleteMenu/AuthCompleteMenu.tsx
+++ packages/app/src/features/discover/components/discoverHeader/components/profileDrawerModal/components/authCompleteMenu/AuthCompleteMenu.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { AuthCompleteMenuProps } from './types';
-import { ActivityIndicator, View } from 'react-native';
+import { View } from 'react-native';
 import { useTheme } from 'src/lib/theme/Theme';
 import stylesCreator from './Styles';
 import { useStrings } from 'src/common/hooks';
 import TextTouchable from 'src/common/components/buttons/textTouchable';
-import { useDiscoverHook } from 'src/features/discover/Hooks';
 
 const AuthCompleteMenu = ({ handleLogout }: AuthCompleteMenuProps) => {
   const [styles] = useTheme(stylesCreator);
   const { AuthComplete } = useStrings().DiscoverScreen.ProfileDrawerMenu;
-  const { loading } = useDiscoverHook();
 
   return (
     <View style={styles.container}>
@@ -73,19 +71,13 @@ const AuthCompleteMenu = ({ handleLogout }: AuthCompleteMenuProps) => {
         />
       </View>
 
-      {loading ? (
-        <View>
-          <ActivityIndicator />
-        </View>
-      ) : (
-        <View style={styles.groupBottom}>
-          <TextTouchable
-            onPress={handleLogout}
-            text={AuthComplete.Buttons.Logout}
-            textStyles={styles.linkButton}
-          />
-        </View>
-      )}
+      <View style={styles.groupBottom}>
+        <TextTouchable
+          onPress={handleLogout}
+          text={AuthComplete.Buttons.Logout}
+          textStyles={styles.linkButton}
+        />
+      </View>
     </View>
   );
 };
