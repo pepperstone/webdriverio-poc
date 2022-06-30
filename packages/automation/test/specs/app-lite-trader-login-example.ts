a03541383ded78f2a00e084a41b36a0b360d06ed
John Aldrieck Nonog <87748465+aldriecknonog@users.noreply.github.com>
2022-06-30 08:44:37 +0800

    feat(percy-web):sample spec file for public website with percy

--- packages/automation/test/specs/app-lite-trader-login-example.ts
+++ packages/automation/test/specs/app-lite-trader-login-example.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const percySnapshot = require('@percy/webdriverio');
+
 import LandingScreen from '../pageobjects/app/landing-screen';
 import LoginScreen from '../pageobjects/app/login-screen';
 import DiscoverScreen from '../pageobjects/app/discover-screen';
@@ -5,11 +8,16 @@ import DiscoverScreen from '../pageobjects/app/discover-screen';
 describe('I should be able to go to in Login Page', () => {
   before('go first to Landing Screen', async () => {
     await LandingScreen.clickLogin();
+    await percySnapshot(driver, 'Landing screen');
   });
   it('should be able to do login in then Login Screen', async () => {
     await LoginScreen.getLogin('test@sample.com', '12345');
+    await percySnapshot(driver, 'Login screen');
   });
   it('should direct to Discover Screen ', async () => {
     await expect(DiscoverScreen.discoverTitle).toBePresent();
+    await percySnapshot(driver, 'Discover');
   });
 });
+
+//setx PERCY_TOKEN "e8d14f148a1e648087b0b9297e55710b2a88dda1be4ff0c0f9ec985804f665c9"
--- packages/automation/test/specs/web-public-website-join-example.ts
+++ packages/automation/test/specs/web-public-website-join-example.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const percySnapshot = require('@percy/webdriverio');
 import HomePage from '../pageobjects/web/home-page';
 
 describe('I should be able to go in SCA-Login Page', () => {
@@ -8,6 +10,9 @@ describe('I should be able to go in SCA-Login Page', () => {
     await expect(HomePage.shortCopyUnit).toBePresent();
   });
   it('should direct to SCA-Login Page ', async () => {
+    await percySnapshot(browser, 'SCU email banner');
     await HomePage.getSignUp('testEmail@myEmail.com');
   });
 });
+
+//setx PERCY_TOKEN "cb03baa5bab17fd5859fd17ccba4d0d1a3f60ce366efa0c18b7a8192c3b5dbb8"
